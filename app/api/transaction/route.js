a3003efd6a574635e04eb87bfd8ae46190ef921e
Maida Eyewear <maidaeyewear@gmail.com>
2024-01-05 16:37:19 +0700

    update kirim wa

--- app/api/transaction/route.js
+++ app/api/transaction/route.js
@@ -8,7 +8,9 @@ import path from 'path';
 import mongoose from "mongoose";
 
 export async function GET(){
-    return NextResponse.json({message: "Get Nota"},{status:201});
+    await connectMongoDB();
+    const transaction = await Transaction.find();
+    return NextResponse.json({transaction},{status:201});
 }
 
 export async function POST(request){
@@ -24,8 +26,9 @@ export async function POST(request){
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     const htmlContent = 
-    `<html><body>
-    <h3>Optik Kustin</h3><hr>
+    `<html><body>   
+    <p align='center'> <b>OPTIK KUSTIN</b><br>Jl. Sirojudin Raya No.37<br>Undip Tembalang - Kota Semarang<br>Telp. 024-76402637 - WA 0813 7757 2015</p>
+    <hr>
     <p><b>No.Nota :</b> ${NoNota}</p>
     <p><b>Tanggal :</b> ${Tanggal}</p>
     <p><b>Keterangan :</b> ${Keterangan}</p>
@@ -33,7 +36,7 @@ export async function POST(request){
     <p><b>No.Handphone :</b> ${NoHandphone}</p>
     </body></html>`;
     await page.setContent(htmlContent);
-    const pdfBuffer = await page.pdf({ format: 'A4', printBackground: true });
+    const pdfBuffer = await page.pdf({ format: 'A6', printBackground: true });
     await browser.close();
 
     // Save the PDF to a file
@@ -46,6 +49,16 @@ export async function POST(request){
     await connectMongoDB();
     await Transaction.create([{NoNota, Tanggal, Keterangan, NamaCustomer, NoHandphone, FilePDF}]);
     mongoose.connection.close()
+
+    //Send PDF to Whatsapp
+    await fetch("http://localhost:8000/transaction",{
+        method: "POST",
+        headers: {
+            "Content-type": "application/json",
+        },
+        body: JSON.stringify({NoNota, Tanggal, Keterangan, NamaCustomer, NoHandphone, FilePDF}),
+    });
+
     //Respon
     return NextResponse.json({message: "Nota Terkirim",Data},{status:201});    
 }
