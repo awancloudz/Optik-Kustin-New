a691e565f84351b7aa7031c541d726e2f55d1b1b
Maida Eyewear <maidaeyewear@gmail.com>
2024-01-13 11:33:00 +0700

    nextconfig

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    domains: [
+      "localhost",
+      "localhost:3000",
+      "103.31.39.135:3000",
+    ],
+}
 
 module.exports = nextConfig
