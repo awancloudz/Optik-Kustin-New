6c15dee40154bdf6d63c8dc6321058ec0591aebb
Maida Eyewear <maidaeyewear@gmail.com>
2024-01-19 16:58:43 +0700

    design nota

--- app/api/pemesananlokal/route.js
+++ app/api/pemesananlokal/route.js
@@ -42,13 +42,13 @@ export async function POST(request){
         
     //Printer Ke Printer Lokal
     const printLokal = () => {        
-        // const electron = typeof process !== 'undefined' && process.versions && !!process.versions.electron;
+        //const electron = typeof process !== 'undefined' && process.versions && !!process.versions.electron;
 
         let printerSO = new ThermalPrinter({
             type: PrinterTypes.EPSON,
-            //interface: 'pos-80',
-            interface: 'tcp://192.168.1.1:9100',
-            // driver: require(electron ? 'electron-printer' : 'printer') 
+            //interface: 'printer: POS80 Printer USB',
+            interface: 'tcp://192.168.1.1',
+            //driver: driverPrint,
         });
         
         var data = `${NoNota}`;         // Barcode data (string or buffer)
@@ -57,23 +57,17 @@ export async function POST(request){
         width: 5,                       // Barcode width
         height: 100,                    // Barcode height (0≤ height ≤255)
         }
-        printerSO.alignCenter();
+        
         printerSO.printBarcode(data, type, settings);
-        printerSO.underline(true);
-        printerSO.drawLine();                                    
-        printerSO.underlineThick(true);                               
-        printerSO.drawLine();
-        printerSO.println();     
+        printerSO.println();   
         printerSO.alignCenter();
-        printerSO.bold(true); 
+        printerSO.setTextDoubleWidth();
         printerSO.println('OPTIK KUSTIN'); 
         printerSO.setTextNormal();
-        printerSO.alignCenter();
         printerSO.println(`Jl. Sirojudin Raya No.37`);
         printerSO.println(`Undip Tembalang - Kota Semarang`);
         printerSO.println(`Telp. 024-76402637 - WA 0813 7757 2015`);
         printerSO.drawLine();
-        printerSO.setTextNormal();
         printerSO.tableCustom([                                      
             { text:"No", align:"LEFT", width:0.3 },
             { text:": "+`${NoNota}`, align:"LEFT", width:0.6 }
@@ -113,13 +107,13 @@ export async function POST(request){
         ]);
         printerSO.drawLine();
         printerSO.tableCustom([          
-            { text:" ", align:"CENTER"},                            
-            { text:"SPH", align:"CENTER"},
-            { text:"CYL", align:"CENTER"},
-            { text:"AXIS", align:"CENTER"},
-            { text:"ADD", align:"CENTER"},
-            { text:"PD", align:"CENTER"},
-            { text:"Vis Akhir", align:"CENTER", width:0.25},
+            { text:"", align:"CENTER"},                            
+            { text:"SPH", align:"CENTER", bold:true},
+            { text:"CYL", align:"CENTER", bold:true},
+            { text:"AXIS", align:"CENTER", bold:true},
+            { text:"ADD", align:"CENTER", bold:true},
+            { text:"PD", align:"CENTER", bold:true},
+            { text:"VisAkhir", align:"CENTER", bold:true, width:0.2},
         ]);
         printerSO.drawLine();
         printerSO.tableCustom([                                      
@@ -129,7 +123,7 @@ export async function POST(request){
             { text:`${Data["AXIS RIGHT"]}`, align:"CENTER"},
             { text:`${Data["ADD"]}`, align:"CENTER"},
             { text:`${Data["PD"]}`, align:"CENTER"},
-            { text: "6/6", align:"CENTER", width:0.25},
+            { text: "6/6", align:"CENTER", width:0.2},
         ]);
         printerSO.tableCustom([                                      
             { text:"L", align:"CENTER"},
@@ -138,7 +132,7 @@ export async function POST(request){
             { text:`${Data["AXIS LEFT"]}`, align:"CENTER"},
             { text: " ", align:"CENTER"},
             { text: " ", align:"CENTER"},
-            { text: " 6/6", align:"CENTER", width:0.25},
+            { text: "6/6", align:"CENTER", width:0.2},
         ]);
         printerSO.drawLine();
         printerSO.tableCustom([                                      
@@ -167,6 +161,7 @@ export async function POST(request){
         ]);
         printerSO.drawLine();
         printerSO.println(); 
+        printerSO.alignLeft();
         printerSO.println('PRECAL :'); 
         printerSO.drawLine();
         printerSO.tableCustom([          
@@ -174,7 +169,7 @@ export async function POST(request){
             { text:"B", align:"CENTER"},
             { text:"DBL", align:"CENTER"},
             { text:"MPD", align:"CENTER"},
-            { text:"SH/PV", align:"CENTER"}
+            { text:"SH/PV", align:"CENTER", width:0.2}
         ]);
         printerSO.drawLine();
         printerSO.tableCustom([          
@@ -182,56 +177,59 @@ export async function POST(request){
             { text:`${Data["B"]}`, align:"CENTER"},
             { text:`${Data["DBL"]}`, align:"CENTER"},
             { text:`${Data["MPD"]}`, align:"CENTER"},
-            { text:`${Data["SH/PV"]}`, align:"CENTER"}
+            { text:`${Data["SH/PV"]}`, align:"CENTER", width:0.2}
         ]);
         printerSO.println(); 
         printerSO.tableCustom([                                      
             { text:"Frame", align:"LEFT", width:0.3 },
-            { text:": "+`${Data.FRAME}`, align:"LEFT", width:0.7 }
+            { text:": "+`${Data.FRAME}`, align:"LEFT", width:0.6 }
         ]);
         printerSO.tableCustom([                                      
             { text:"Lensa", align:"LEFT", width:0.3 },
-            { text:": "+`${Data.LENSA}`, align:"LEFT", width:0.7 }
+            { text:": "+`${Data.LENSA}`, align:"LEFT", width:0.6 }
         ]);
         printerSO.tableCustom([                                      
             { text:"Total", align:"LEFT", width:0.3 },
-            { text:": "+`${Data.TOTAL}`, align:"LEFT", width:0.7 }
+            { text:": "+`${Data.TOTAL}`, align:"LEFT", width:0.6 }
         ]);
         printerSO.tableCustom([                                      
             { text:"Uang Muka", align:"LEFT", width:0.3 },
-            { text:": "+`${Data.UANGMUKA}`, align:"LEFT", width:0.7 }
+            { text:": "+`${Data.UANGMUKA}`, align:"LEFT", width:0.6 }
         ]);
         printerSO.tableCustom([                                      
             { text:"Sisa", align:"LEFT", width:0.3 },
-            { text:": "+`${Data.LENSA}`, align:"LEFT", width:0.7 }
+            { text:": "+`${Data.LENSA}`, align:"LEFT", width:0.6 }
         ]);
         printerSO.println(); 
-        printerSO.bold(true); 
+        printerSO.println(); 
+        printerSO.setTextDoubleWidth();
+        printerSO.alignCenter();
         printerSO.println(`${Cabang}`); 
+        printerSO.setTextNormal();
         printerSO.println(); 
         printerSO.tableCustom([                                      
-            { text:"Edger", align:"CENTER", width:0.5 },
-            { text:"Quality Control", align:"CENTER", width:0.5 }
+            { text:"Edger", align:"CENTER", width:0.45 },
+            { text:"Quality Control", align:"CENTER", width:0.45 }
         ]);
         printerSO.println();
         printerSO.println();
         printerSO.println();
         printerSO.tableCustom([  
-            { text:"(...............)", align:"CENTER", width:0.5 },
-            { text:"(...............)", align:"CENTER", width:0.5 },
+            { text:"(...............)", align:"CENTER", width:0.45 },
+            { text:"(...............)", align:"CENTER", width:0.45 },
         ]);
         printerSO.println();
         printerSO.println();
         printerSO.tableCustom([                                      
-            { text:"Yang Menyerahkan", align:"CENTER", width:0.5 },
-            { text:"Penerima", align:"CENTER", width:0.5 }
+            { text:"Yang Menyerahkan", align:"CENTER", width:0.45 },
+            { text:"Penerima", align:"CENTER", width:0.45 }
         ]);
         printerSO.println();
         printerSO.println();
         printerSO.println();
         printerSO.tableCustom([  
-            { text:"(...............)", align:"CENTER", width:0.5 },
-            { text:"(...............)", align:"CENTER", width:0.5 },
+            { text:"(...............)", align:"CENTER", width:0.45 },
+            { text:"(...............)", align:"CENTER", width:0.45 },
         ]);
         printerSO.cut();
 
